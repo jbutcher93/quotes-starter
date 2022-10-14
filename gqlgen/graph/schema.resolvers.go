4b1d5a45e8759e229fd0aa8b5b7f0f24629046f9
John Butcher <1butcherjohn@gmail.com>
2022-10-14 10:32:18 -0400

    makeRequest func utilizing DRY principle for validating on various queries

--- gqlgen/graph/schema.resolvers.go
+++ gqlgen/graph/schema.resolvers.go
@@ -13,16 +13,19 @@ import (
 	"github.com/jbutcher93/quotes-starter/gqlgen/graph/model"
 )
 
+func makeRequest(url string, requestType string) *http.Response {
+	client := &http.Client{}
+	req, _ := http.NewRequest(requestType, url, nil)
+	req.Header.Set("X-Api_Key", "COCKTAILSAUCE")
+	response, _ := client.Do(req)
+	return response
+}
+
 // RandomQuote is the resolver for the randomQuote field.
 func (r *queryResolver) RandomQuote(ctx context.Context) (*model.Quote, error) {
-	response, _ := http.Get("http://0.0.0.0:8082/quotes")
+	response := makeRequest("http://0.0.0.0:8082/quotes", "GET")
 	responseData, _ := io.ReadAll(response.Body)
 	var randomQuote *model.Quote
-	// randomQuote = &model.Quote{
-	// 	ID:     "123",
-	// 	Author: "abc",
-	// 	Phrase: "xyz",
-	// }
 	json.Unmarshal(responseData, &randomQuote)
 	return randomQuote, nil
 }
