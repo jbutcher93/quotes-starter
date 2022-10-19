1deb3a4618ea4766aea763d324c990db9981f601
John Butcher <1butcherjohn@gmail.com>
2022-10-18 21:15:41 -0400

    Added variable to auth.go to clarify what is ctxKey and ctxValue

--- Auth/auth.go
+++ Auth/auth.go
@@ -10,9 +10,10 @@ func Middleware() func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			ctxKey := "X-Api-Key"
+			ctxValue := r.Header.Get("X-Api-Key")
 
 			// put it in context
-			ctx := context.WithValue(r.Context(), ctxKey, r.Header.Get("X-Api-Key"))
+			ctx := context.WithValue(r.Context(), ctxKey, ctxValue)
 
 			// and call the next with our new context
 			r = r.WithContext(ctx)
