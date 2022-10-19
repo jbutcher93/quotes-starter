5f48251ce1ddb20a213c77d6b899a960a8428b6b
Erica Handelman <erica.handelman@gmail.com>
2022-10-19 15:37:47 -0400

    remove blank line

--- Auth/auth.go
+++ Auth/auth.go
@@ -8,7 +8,6 @@ import (
 func Middleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 			ctxKey := "X-Api-Key"
 			ctxValue := r.Header.Get("X-Api-Key")
 
