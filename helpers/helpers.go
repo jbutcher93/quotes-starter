839e4431ee4cf127f52ce0aba66c7173e3ec044a
John Butcher <1butcherjohn@gmail.com>
2022-10-20 11:31:00 -0400

    Added additional error-handling

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -8,20 +8,27 @@ import (
 	"net/http"
 )
 
-func MakeRequest(ctx context.Context, url string, requestType string, body io.Reader) *http.Response {
-	auth := fmt.Sprint(ctx.Value("X-Api-Key"))
+func MakeRequest(ctx context.Context, url string, requestType string, body io.Reader) (*http.Response, error) {
+	headerValue := fmt.Sprint(ctx.Value("X-Api-Key"))
 	client := &http.Client{}
-	req, _ := http.NewRequest(requestType, url, body)
-	req.Header.Set("X-Api-Key", auth)
+	req, err := http.NewRequest(requestType, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("X-Api-Key", headerValue)
 	req.Header.Set("Content-Type", "application/json")
-	response, _ := client.Do(req)
-	return response
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 func HandleResponse(r *http.Response) ([]byte, error) {
-	if r.StatusCode == 401 {
+	if r.StatusCode == 401 || r.StatusCode == 404 {
 		return nil, errors.New("error: " + r.Status)
-	} else {
+	}
+	{
 		responseData, err := io.ReadAll(r.Body)
 		return responseData, err
 	}
